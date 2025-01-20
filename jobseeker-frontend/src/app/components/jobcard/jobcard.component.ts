51937b8861f70987095fe665ba36de87941db9a3
Matthieu Mestrez <matthieu.mestrez@capyx.be>
2025-01-20 14:48:02 +0100

    Removed unnecessary code

--- jobseeker-frontend/src/app/components/jobcard/jobcard.component.ts
+++ jobseeker-frontend/src/app/components/jobcard/jobcard.component.ts
@@ -3,29 +3,20 @@ import { JobPost } from '../../models/job.posts';
 import { JobStatus } from '../../models/job.status';
 import { CommonModule } from '@angular/common';
 import { MarkdownModule } from 'ngx-markdown';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'jobcard',
-  imports: [CommonModule, MarkdownModule, HttpClientModule],
+  imports: [CommonModule, MarkdownModule],
   templateUrl: './jobcard.component.html',
   styleUrl: './jobcard.component.css'
 })
 export class JobcardComponent {
-  @Input() job!: JobPost;  // Input the job data
+  @Input() job!: JobPost;
   JobStatus = JobStatus;
   expandedJobId: number | undefined = undefined;
 
-  // Property to hold the rendered markdown
-  renderedDescription: string | Promise<String>;
-
-  constructor() {
-    this.renderedDescription = ''; // Initialize the rendered description
-  }
-
-
-  handleReadMoreToggle(jobId: number | undefined): void {
-    this.expandedJobId = this.expandedJobId === jobId ? undefined : jobId;
+  handleReadMoreToggle(): void {
+    this.expandedJobId = this.expandedJobId === this.job?.id ? undefined : this.job?.id;
   }
 
 
