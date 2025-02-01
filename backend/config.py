2fa918493bf3997cd74940d3a43fa83c6fc074b6
TheSuperBeaver <mestrez.matthieu@gmail.com>
2025-02-01 17:29:23 +0100

    Take database params from os config

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,10 @@ import os
 
 class Config:
     MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://jobspy:jobspy@{MYSQL_HOST}/jobspy"
+    MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
+    MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "jobspy")
+    MYSQL_USER = os.getenv("MYSQL_USER", "jobspy")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "jobspy")
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
