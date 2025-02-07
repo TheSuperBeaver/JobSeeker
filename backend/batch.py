46432716e69bd114fabe8e76c339f5a1f1fa2144
TheSuperBeaver <mestrez.matthieu@gmail.com>
2025-02-07 14:26:19 +0100

    Fix of the batch connection to the database

--- backend/batch.py
+++ backend/batch.py
@@ -1,58 +1,67 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+from flask import Flask
 from jobspy import scrape_jobs
 from db.db_utils import (
     get_all_automatic_jobqueries,
     insert_jobs_into_db,
     get_country_by_code,
 )
+from db.db_model import db
+from config import Config
 import time
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
 
 
 def check_job_queries():
-    print("Checking for scheduled job queries...")
-    try:
-        jobs = get_all_automatic_jobqueries()
+    with app.app_context():
+        print("Checking for scheduled job queries...")
+        try:
+            jobs = get_all_automatic_jobqueries()
 
-        for job in jobs:
-            if job.get("query_google") == 1:
-                job_results = scrape_jobs(
-                    site_name="google",
-                    search_term=job.get("search_term"),
-                    location=f"{job.get('town')}, {job.get('country')}",
-                    locations=[f"{job.get('town')}, {job.get('country')}"],
-                    filter_by_title=[],
-                    results_wanted=job.get("results", 0),
-                    hours_old=24,
-                )
-                insert_jobs_into_db(job_results)
-            if job.get("query_indeed") == 1:
-                job_results = scrape_jobs(
-                    site_name="indeed",
-                    search_term=job.get("search_term"),
-                    location=f"{job.get('town')}, {job.get('country')}",
-                    locations=[f"{job.get('town')}, {job.get('country')}"],
-                    filter_by_title=[],
-                    results_wanted=job.get("results", 0),
-                    hours_old=24,
-                    country_indeed=get_country_by_code(job.get("country")),
-                )
-                insert_jobs_into_db(job_results)
-            if job.get("query_linkedin") == 1:
-                job_results = scrape_jobs(
-                    site_name="linkedin",
-                    search_term=job.get("search_term"),
-                    location=f"{job.get('town')}, {job.get('country')}",
-                    locations=[f"{job.get('town')}, {job.get('country')}"],
-                    filter_by_title=[],
-                    results_wanted=job.get("results", 0),
-                    hours_old=24,
-                )
-                insert_jobs_into_db(job_results)
+            for job in jobs:
+                if job.get("query_google") == 1:
+                    job_results = scrape_jobs(
+                        site_name="google",
+                        search_term=job.get("search_term"),
+                        location=f"{job.get('town')}, {job.get('country')}",
+                        locations=[f"{job.get('town')}, {job.get('country')}"],
+                        filter_by_title=[],
+                        results_wanted=job.get("results", 0),
+                        hours_old=24,
+                    )
+                    insert_jobs_into_db(job_results)
+                if job.get("query_indeed") == 1:
+                    job_results = scrape_jobs(
+                        site_name="indeed",
+                        search_term=job.get("search_term"),
+                        location=f"{job.get('town')}, {job.get('country')}",
+                        locations=[f"{job.get('town')}, {job.get('country')}"],
+                        filter_by_title=[],
+                        results_wanted=job.get("results", 0),
+                        hours_old=24,
+                        country_indeed=get_country_by_code(job.get("country")),
+                    )
+                    insert_jobs_into_db(job_results)
+                if job.get("query_linkedin") == 1:
+                    job_results = scrape_jobs(
+                        site_name="linkedin",
+                        search_term=job.get("search_term"),
+                        location=f"{job.get('town')}, {job.get('country')}",
+                        locations=[f"{job.get('town')}, {job.get('country')}"],
+                        filter_by_title=[],
+                        results_wanted=job.get("results", 0),
+                        hours_old=24,
+                    )
+                    insert_jobs_into_db(job_results)
 
-        print("Job check completed.")
-    except Exception as e:
-        print(f"Error checking job queries: {e}")
+            print("Job check completed.")
+        except Exception as e:
+            print(f"Error checking job queries: {e}")
 
 
 if __name__ == "__main__":
