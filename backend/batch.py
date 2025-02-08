87b54dcf0f42c7a5e304889857b7ffcd89359bb6
TheSuperBeaver <mestrez.matthieu@gmail.com>
2025-02-08 01:18:05 +0100

    Fixed batch

--- backend/batch.py
+++ backend/batch.py
@@ -11,10 +11,16 @@ from db.db_model import db
 from config import Config
 import time
 import logging
+import sys
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout)  # Ensure logs are printed to stdout
+    ],
 )
+
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
@@ -30,40 +36,40 @@ def check_job_queries():
             jobs = get_all_automatic_jobqueries()
 
             for job in jobs:
-                if job.get("query_google") == 1:
+                if job.query_google == 1:
                     job_results = scrape_jobs(
                         site_name="google",
-                        search_term=job.get("search_term"),
-                        location=f"{job.get('town')}, {job.get('country')}",
-                        locations=[f"{job.get('town')}, {job.get('country')}"],
+                        search_term=job.search_term,
+                        location=f"{job.town}, {job.country}",
+                        locations=[f"{job.town}, {job.country}"],
                         filter_by_title=[],
-                        results_wanted=job.get("results", 0),
+                        results_wanted=job.results,
                         hours_old=24,
                     )
-                    insert_jobs_into_db(job_results)
-                if job.get("query_indeed") == 1:
+                    insert_jobs_into_db(job_results, job.id)
+                if job.query_indeed == 1:
                     job_results = scrape_jobs(
                         site_name="indeed",
-                        search_term=job.get("search_term"),
-                        location=f"{job.get('town')}, {job.get('country')}",
-                        locations=[f"{job.get('town')}, {job.get('country')}"],
+                        search_term=job.search_term,
+                        location=f"{job.town}, {job.country}",
+                        locations=[f"{job.town}, {job.country}"],
                         filter_by_title=[],
-                        results_wanted=job.get("results", 0),
+                        results_wanted=job.results,
                         hours_old=24,
-                        country_indeed=get_country_by_code(job.get("country")),
+                        country_indeed=get_country_by_code(job.country),
                     )
-                    insert_jobs_into_db(job_results)
-                if job.get("query_linkedin") == 1:
+                    insert_jobs_into_db(job_results, job.id)
+                if job.query_linkedin == 1:
                     job_results = scrape_jobs(
                         site_name="linkedin",
-                        search_term=job.get("search_term"),
-                        location=f"{job.get('town')}, {job.get('country')}",
-                        locations=[f"{job.get('town')}, {job.get('country')}"],
+                        search_term=job.search_term,
+                        location=f"{job.town}, {job.country}",
+                        locations=[f"{job.town}, {job.country}"],
                         filter_by_title=[],
-                        results_wanted=job.get("results", 0),
+                        results_wanted=job.results,
                         hours_old=24,
                     )
-                    insert_jobs_into_db(job_results)
+                    insert_jobs_into_db(job_results, job.id)
 
             logger.info("Job check completed.")
         except Exception as e:
@@ -71,14 +77,17 @@ def check_job_queries():
 
 
 if __name__ == "__main__":
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(check_job_queries, CronTrigger(hour="*"))
+    with app.app_context():
+        scheduler = BackgroundScheduler(daemon=False)
+        scheduler.add_job(check_job_queries, CronTrigger(hour="*"))
 
-    try:
-        scheduler.start()
-        logger.info("Scheduler started. Waiting for tasks...")
-        while True:
-            time.sleep(60)
-    except (KeyboardInterrupt, SystemExit):
-        scheduler.shutdown()
-        logger.info("Scheduler stopped.")
+        try:
+            scheduler.start()
+            logger.info("Scheduler started. Waiting for tasks...")
+            while True:
+                time.sleep(60)
+                sys.stdout.flush()
+                sys.stderr.flush()
+        except (KeyboardInterrupt, SystemExit):
+            scheduler.shutdown()
+            logger.info("Scheduler stopped.")
